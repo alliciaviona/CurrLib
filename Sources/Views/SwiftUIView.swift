11f8e9430b31c1b910c2fb63ff3dcb1204dbabb1
Allicia Viona <alliciawn@gmail.com>
2023-07-13 14:56:20 +0700

    Add text in content view

---
+++ Sources/Views/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Allicia Viona Sagi on 13/07/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("This is from package manager!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
