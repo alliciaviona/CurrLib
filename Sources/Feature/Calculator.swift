0de2e150b9ba3fcb5b55d4c8a060359215b3a42f
Allicia Viona <alliciawn@gmail.com>
2023-07-13 15:21:07 +0700

    Add calculator function

---
+++ Sources/Feature/Calculator.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Allicia Viona Sagi on 13/07/23.
+//
+
+import Foundation
+
+class Calculator {
+    
+    func add(valueA: Int, valueB: Int) -> Int {
+        return valueA + valueB
+    }
+    
+    func subtract(valueA: Int, valueB: Int) -> Int {
+        return valueA - valueB
+    }
+    
+    func multiply(valueA: Int, valueB: Int) -> Int {
+        return valueA * valueB
+    }
+    
+    func divide(valueToDivide: Int, divisor: Int) -> Int {
+        return valueToDivide / divisor
+    }
+    
+}
